fe94df15d1464339175c50510ac8f50083f16ca7
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2021-01-01 10:11:32 +0200

    Update health-dump.js

--- scripts/bnf/health-dump.js
+++ scripts/bnf/health-dump.js
@@ -1,4 +1,4 @@
 // Just dump the health aleardy
 
-Vars.content.units().each(h=>h.health = Number.MAX-VALUE);
-Vars.content.blocks().each(i=>i.health = Number.MAX-VALUE);
+Vars.content.units().each(h=>h.health = Number.MAX_VALUE);
+Vars.content.blocks().each(i=>i.health = Number.MAX_VALUE);
